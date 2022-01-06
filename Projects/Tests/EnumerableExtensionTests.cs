515f504c3ba2e9aa2f86e335c0d66a344e060923
Martin Gleich <martin.gleich3@web.de>
2022-01-06 15:33:04 +0100

    Remove old tests

--- Projects/Tests/EnumerableExtensionTests.cs
+++ Projects/Tests/EnumerableExtensionTests.cs
@@ -9,44 +9,8 @@ using System.Collections.Immutable;
 
 namespace Tests
 {
-	public static class Ext
-	{
-		public static IEnumerable<T> Checked<T>(this IEnumerable<T> inner, int maxCount)
-		{
-			int cur = 0;
-			foreach (var x in inner)
-			{
-				if (cur == maxCount)
-					throw new InvalidOperationException("Read to many values from checked enumerable");
-				yield return x;
-				++cur;
-			}
-		}
-	}
 	public static class EnumerableExtensionTests
 	{
-
-		[Fact]
-		public static void TryGetSingle_Null()
-		{
-			Assert.Throws<ArgumentNullException>(() => EnumerableExtensions.TryGetSingle<int>(null!, out _));
-		}
-		[Fact]
-		public static void TryGetSingle_None()
-		{
-			Assert.False(EnumerableExtensions.TryGetSingle(Array.Empty<int>(), out _));
-		}
-		[Fact]
-		public static void TryGetSingle_One()
-		{
-			Assert.True(EnumerableExtensions.TryGetSingle(new[] { 7 }, out var x));
-			Assert.Equal(7, x);
-		}
-		[Fact]
-		public static void TryGetSingle_Three()
-		{
-			Assert.False(EnumerableExtensions.TryGetSingle(new[] { 7, 8, 3 }.Checked(2), out _));
-		}
 		[Fact]
 		public static void ImmutableTryGetSingle_None()
 		{
@@ -64,32 +28,6 @@ namespace Tests
 			Assert.False(EnumerableExtensions.TryGetSingle(ImmutableArray.Create(7, 8), out _));
 		}
 
-		[Fact]
-		public static void MoreThan_NullArg()
-		{
-			Assert.Throws<ArgumentNullException>(() => EnumerableExtensions.MoreThan<int>(null!, 1));
-		}
-		[Fact]
-		public static void MoreThan_Zero()
-		{
-			Assert.False(EnumerableExtensions.MoreThan(Array.Empty<int>(), 1));
-		}
-		[Fact]
-		public static void MoreThan_One()
-		{
-			Assert.False(EnumerableExtensions.MoreThan(new int[] { 6 }, 1));
-		}
-		[Fact]
-		public static void MoreThan_Two()
-		{
-			Assert.True(EnumerableExtensions.MoreThan(new int[] { 6, 7 }.Checked(2), 1));
-		}
-		
-		[Fact]
-		public static void MoreThan_Three()
-		{
-			Assert.True(EnumerableExtensions.MoreThan(new int[] { 6, 7, 8 }.Checked(2), 1));
-		}
 
 		[Fact]
 		public static void HasNoNullElement_Null()
